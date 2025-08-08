f34d93809440e53f0e1a385e9ad03d5e2768eae9
atibzo <atib@zo.xyz>
2025-08-08 20:36:35 +0530

    Create root page with redirect to demo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1bfdcbf192fc8664"; }
+/******/ 		__webpack_require__.h = function() { return "0c5021b9360e7ec1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
